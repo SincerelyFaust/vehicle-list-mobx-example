8ba2c05da8c4b317366f719fc6bb11e413cf84b2
SincerelyFaust <hedesmarin@gmail.com>
2023-07-30 18:09:10 +0200

    fix(ListLayout, ListItem): change to <ul> and <li>

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -2,13 +2,13 @@ import styles from "@/components/ListItem.module.css";
 
 export default function ListItem({ make, model }) {
   return (
-    <div className={styles["list-item"]}>
+    <li className={styles["list-item"]}>
       <p>{make}</p>
       <p>{model}</p>
       <div className={styles["button-div"]}>
         <button onClick={() => {}}>Uredi</button>
         <button onClick={() => {}}>Izbriši</button>
       </div>
-    </div>
+    </li>
   );
 }
--- layouts/ListLayout.jsx
+++ layouts/ListLayout.jsx
@@ -1,5 +1,5 @@
 import styles from "@/layouts/ListLayout.module.css";
 
 export default function ListLayout({ children }) {
-  return <div className={styles["list-layout"]}>{children}</div>;
+  return <ul className={styles["list-layout"]}>{children}</ul>;
 }
