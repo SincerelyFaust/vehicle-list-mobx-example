d81c855c747144c519b2eb7ed13b23db94ad0664
SincerelyFaust <hedesmarin@gmail.com>
2023-08-07 15:26:44 +0200

    feat(Dialog): close on outer click
    simple code that basically closes the dialog when a user clicks outside of it

--- components/Dialog.jsx
+++ components/Dialog.jsx
@@ -1,9 +1,27 @@
 import styles from "@/components/Dialog.module.css";
+import { useRef, useEffect } from "react";
 
 export default function Dialog({ open, title, children, form, resetState }) {
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dialogRef.current && !dialogRef.current.contains(event.target)) {
+        resetState();
+      }
+    };
+
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }
+  }, [resetState, open]);
+
   return (
     <div hidden={!open} className={styles["dialog-container"]}>
-      <dialog hidden={!open} className={styles["dialog"]}>
+      <dialog ref={dialogRef} hidden={!open} className={styles["dialog"]}>
         <p className={styles["dialog-title"]}>{title}</p>
         {children}
         <div className={styles["dialog-buttons-container"]}>
