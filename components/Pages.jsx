58f9273863bd9c7b7bd980093f279550a86c9439
SincerelyFaust <hedesmarin@gmail.com>
2023-08-09 02:00:27 +0200

    fix(Pages): redesign
    this redesign improves UX because if we had more than even 10 pages there would be 10 buttons + 2 for forward and backwards, so 12 in total, which is a lot. it would simply not scale well :)

--- components/Pages.jsx
+++ components/Pages.jsx
@@ -1,4 +1,6 @@
 import styles from "@/components/Pages.module.css";
+import { ArrowRight, ArrowLeft } from "lucide-react";
+import { useState } from "react";
 
 export default function Pages({
   itemCount,
@@ -7,39 +9,53 @@ export default function Pages({
   onPageChange,
 }) {
   const pagesCount = Math.ceil(itemCount / displayItems);
+  const [page, setPage] = useState(+currentPage);
 
-  const pages = Array.from({ length: pagesCount }, (_, i) => i + 1);
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (+page <= pagesCount && +page > 0) onPageChange(+page);
+  }
 
   return (
     <div className={styles["pages"]}>
       <button
-        className={styles["page"]}
+        className={styles["page-button"]}
         onClick={() => {
-          currentPage > 1 ? onPageChange(currentPage - 1) : null;
+          if (currentPage > 1) {
+            onPageChange(currentPage - 1);
+            setPage(currentPage - 1);
+          }
         }}
       >
-        Prethodna
+        <ArrowLeft size={16} />
       </button>
-      <div className={styles["page-buttons-div"]}>
-        {pages.map((page) => (
-          <button
-            key={page}
-            className={
-              page === currentPage ? styles["active-page"] : styles["page"]
-            }
-            onClick={() => onPageChange(page)}
-          >
-            {page}
-          </button>
-        ))}
-      </div>
+      <form
+        className={styles["change-page-form"]}
+        id="page-form"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className={styles["change-page-input"]}
+          type="number"
+          name="page_input"
+          id="page_input"
+          value={page}
+          form="page-form"
+          onChange={(e) => setPage(e.target.value)}
+        />
+        <p>/ {pagesCount}</p>
+      </form>
       <button
-        className={styles["page"]}
+        className={styles["page-button"]}
         onClick={() => {
-          pagesCount !== currentPage ? onPageChange(currentPage + 1) : null;
+          if (pagesCount !== currentPage) {
+            onPageChange(currentPage + 1);
+            setPage(currentPage + 1);
+          }
         }}
       >
-        Sljedeća
+        <ArrowRight size={16} />
       </button>
     </div>
   );
