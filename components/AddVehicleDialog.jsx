b9f02ee8ea2deb11907cce396315eba3a8d5fbab
SincerelyFaust <hedesmarin@gmail.com>
2023-08-25 03:15:27 +0200

    fix(AddVehicleDialog): move all business logic to store

--- components/AddVehicleDialog.jsx
+++ components/AddVehicleDialog.jsx
@@ -4,130 +4,30 @@ import { useStore } from "@/common/StoreProvider";
 import styles from "@/components/StyledDialog.module.css";
 import CustomSelect from "@/components/CustomSelect";
 import { Car } from "lucide-react";
-import { HttpClient } from "@/common/HttpClient";
-import { ModelService } from "@/common/ModelService";
-import { MakeService } from "@/common/MakeService";
 import { observer, useLocalObservable } from "mobx-react-lite";
-import { AddVehicleForm } from "@/common/AddVehicleForm";
 import StyledInput from "./StyledInput";
+import AddVehicleStore from "@/stores/AddVehicleStore";
 
 export default observer(function AddVehicleDialog({ open, setOpen }) {
-  const store = useStore();
-  const httpClient = new HttpClient();
-  const modelService = new ModelService(httpClient);
-  const makeService = new MakeService(httpClient);
-  const form = useLocalObservable(() => new AddVehicleForm());
-
-  const selectMake = useLocalObservable(() => ({
-    id: 0,
-    name: "",
-    setId(value) {
-      this.id = value;
-    },
-    setName(value) {
-      this.name = value;
-    },
-  }));
-
-  const error = useLocalObservable(() => ({
-    message: "",
-    setMessage(value) {
-      this.message = value;
-    },
-  }));
+  const vehicleStore = useStore();
+  const addVehicleStore = useLocalObservable(
+    () => new AddVehicleStore(vehicleStore, setOpen)
+  );
 
   const vehicleMakeOptions = {
     Dodaj: [{ value: 0, label: "Dodaj" }],
     "Postojeće marke": [
-      ...store.VehicleMake.map((make) => ({
+      ...vehicleStore.VehicleMake.map((make) => ({
         value: make.id,
         label: make.abrv,
       })),
     ],
   };
 
-  function findMake(makeId) {
-    const vehicleMake = store.VehicleMake.find((make) => make.id === makeId);
-
-    if (vehicleMake) {
-      form.$("makeName").set(vehicleMake.name);
-      form.$("makeAbrv").set(vehicleMake.abrv);
-    }
-  }
-
-  function handleMakeSelectChange(value) {
-    if (value === 0) {
-      selectMake.setId(0);
-      form.$("makeName").set("");
-      form.$("makeAbrv").set("");
-    } else {
-      selectMake.setId(value);
-      findMake(value);
-    }
-  }
-
-  function resetState() {
-    setOpen(!open);
-    form.reset();
-    selectMake.setId(0);
-    selectMake.setName("");
-    error.setMessage("");
-  }
-
   async function handleSubmit(event) {
     event.preventDefault();
 
-    form.validate();
-
-    if (form.isValid) {
-      const makeWithHighestId =
-        store.VehicleMake.length > 0
-          ? Math.max(...store.VehicleMake.map((make) => make.id))
-          : 0;
-
-      const modelWithHighestId =
-        store.VehicleModel.length > 0
-          ? Math.max(...store.VehicleModel.map((model) => model.id))
-          : 0;
-
-      const data = {
-        make: {
-          id: makeWithHighestId + 1,
-          name: form.values().makeName,
-          abrv: form.values().makeAbrv,
-        },
-        model: {
-          id: modelWithHighestId + 1,
-          name: form.values().modelName,
-          abrv: form.values().modelAbrv,
-          makeid: selectMake.id === 0 ? makeWithHighestId + 1 : selectMake.id,
-        },
-      };
-
-      if (selectMake.id === 0) {
-        const makeResponse = await makeService.addMake(data.make);
-
-        if (makeResponse) {
-          return error.setMessage(makeResponse);
-        }
-
-        store.addMakeToStore(data.make);
-      }
-
-      const modelResponse = await modelService.addModel(data.model);
-
-      if (modelResponse) {
-        return error.setMessage(modelResponse);
-      }
-
-      store.addModelToStore(data.model);
-
-      form.onSubmit();
-
-      resetState();
-    } else {
-      error.setMessage("Molimo ispunita sva polja u obrascu.");
-    }
+    await addVehicleStore.addVehicle();
   }
 
   return (
@@ -135,8 +35,8 @@ export default observer(function AddVehicleDialog({ open, setOpen }) {
       title={"Dodaj novo vozilo"}
       open={open}
       form={"add-car-form"}
-      resetState={resetState}
-      error={error.message}
+      resetState={addVehicleStore.resetState}
+      error={addVehicleStore.error}
     >
       <Form handleSubmit={handleSubmit} formId={"add-car-form"}>
         <label>Marka vozila</label>
@@ -148,22 +48,23 @@ export default observer(function AddVehicleDialog({ open, setOpen }) {
                 icon: <Car size={14} />,
               }}
               options={[vehicleMakeOptions]}
-              selectedOption={selectMake.name}
-              setSelectedOption={selectMake.setName}
-              onChange={handleMakeSelectChange}
+              selectedOption={addVehicleStore.selectMake.name}
+              setSelectedOption={addVehicleStore.setSelectMakeName}
+              onChange={addVehicleStore.handleMakeSelectChange}
             />
           </div>
-          {selectMake.id === 0 || store.VehicleMake.length < 1 ? (
+          {addVehicleStore.selectMake.id === 0 ||
+          vehicleStore.VehicleMake.length < 1 ? (
             <>
-              <StyledInput field={form.$("makeName")} />
-              <StyledInput field={form.$("makeAbrv")} />
+              <StyledInput field={addVehicleStore.form.$("makeName")} />
+              <StyledInput field={addVehicleStore.form.$("makeAbrv")} />
             </>
           ) : null}
         </div>
         <label>Model vozila</label>
         <div className={styles["form-content"]}>
-          <StyledInput field={form.$("modelName")} />
-          <StyledInput field={form.$("modelAbrv")} />
+          <StyledInput field={addVehicleStore.form.$("modelName")} />
+          <StyledInput field={addVehicleStore.form.$("modelAbrv")} />
         </div>
       </Form>
     </Dialog>
---
+++ stores/AddVehicleStore.jsx
@@ -0,0 +1,118 @@
+import { makeObservable, observable, action } from "mobx";
+import { AddVehicleForm } from "@/common/AddVehicleForm";
+import { ModelService } from "@/common/ModelService";
+import { MakeService } from "@/common/MakeService";
+import { HttpClient } from "@/common/HttpClient";
+
+export default class AddVehicleStore {
+  form;
+  selectMake = {
+    id: 0,
+    name: "",
+  };
+  error = "";
+  httpClient;
+  modelService;
+  makeService;
+  vehicleStore;
+  setOpen;
+
+  constructor(vehicleStore, setOpen) {
+    this.vehicleStore = vehicleStore;
+    this.form = new AddVehicleForm();
+    this.httpClient = new HttpClient();
+    this.modelService = new ModelService(this.httpClient);
+    this.makeService = new MakeService(this.httpClient);
+    this.setOpen = setOpen;
+
+    makeObservable(this, {
+      form: observable,
+      selectMake: observable,
+      error: observable,
+      findMake: action.bound,
+      handleMakeSelectChange: action.bound,
+      resetState: action.bound,
+      addVehicle: action.bound,
+      setSelectMakeName: action.bound,
+    });
+  }
+
+  setSelectMakeName(value) {
+    this.selectMake.name = value;
+  }
+
+  findMake(makeId) {
+    const vehicleMake = this.vehicleStore.VehicleMake.find(
+      (make) => make.id === makeId
+    );
+    if (vehicleMake) {
+      this.form.$("makeName").set(vehicleMake.name);
+      this.form.$("makeAbrv").set(vehicleMake.abrv);
+    }
+  }
+
+  handleMakeSelectChange(value) {
+    if (value === 0) {
+      this.selectMake.id = 0;
+      this.form.$("makeName").set("");
+      this.form.$("makeAbrv").set("");
+    } else {
+      this.selectMake.id = value;
+      this.findMake(value);
+    }
+  }
+
+  resetState() {
+    this.setOpen(false);
+    this.form.reset();
+    this.selectMake.id = 0;
+    this.selectMake.name = "";
+    this.error = "";
+  }
+
+  getHighestId(arr) {
+    return arr.length > 0 ? Math.max(...arr.map((item) => item.id)) : 0;
+  }
+
+  async addVehicle() {
+    this.form.validate();
+
+    if (this.form.isValid)
+      return (this.error = "Molimo ispunita sva polja u obrascu.");
+
+    const makeHighestId = this.getHighestId(this.vehicleStore.VehicleMake);
+    const modelHighestId = this.getHighestId(this.vehicleStore.VehicleModel);
+
+    const data = {
+      make: {
+        id: makeHighestId + 1,
+        name: this.form.values().makeName,
+        abrv: this.form.values().makeAbrv,
+      },
+      model: {
+        id: modelHighestId + 1,
+        name: this.form.values().modelName,
+        abrv: this.form.values().modelAbrv,
+        makeid:
+          this.selectMake.id === 0 ? makeHighestId + 1 : this.selectMake.id,
+      },
+    };
+
+    if (this.selectMake.id === 0) {
+      const makeResponse = await this.makeService.addMake(data.make);
+      if (makeResponse) {
+        return (this.error = makeResponse);
+      }
+      this.vehicleStore.addMakeToStore(data.make);
+    }
+
+    const modelResponse = await this.modelService.addModel(data.model);
+    if (modelResponse) {
+      return (this.error = modelResponse);
+    }
+
+    this.vehicleStore.addModelToStore(data.model);
+    this.form.onSubmit();
+    this.resetState();
+  }
+}
